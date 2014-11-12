343522a66d7b6f431c537653cf1b7c7bf30d8a66
Sergio Ramirez <hr@SergioRamirez.local>
2014-11-12 22:32:12 +0100

    Added OmniAuth to logging

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,3 +1,5 @@
+OmniAuth.config.logger = Rails.logger
+
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :twitter, Rails.application.secrets.omniauth_provider_key, Rails.application.secrets.omniauth_provider_secret
 end
